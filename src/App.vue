7ad95bfc34810dc36604b0d39da48511d067ee3c
Hassan Farhat <plexofdeath@gmail.com>
2023-09-01 07:43:24 +1000

    --initial commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref, onMounted, computed, watch } from 'vue'
+
+interface IToDoListItem {
+    id: number
+    content: string
+    category: string
+    isDone: boolean
+    createdAt: Date
+}
+
+const todos = ref<IToDoListItem[]>([])
+const name = ref('')
+
+const input_content = ref('')
+
+const todos_asc = computed(() => todos.value.sort((a, b) => a.id - b.id))
+
+const addToDo = () => {
+    if (input_content.value.trim() === '') {
+        return
+    }
+
+    const newTodo: IToDoListItem = {
+        id: findHighestID() + 1,
+        content: input_content.value,
+        category: 'general',
+        isDone: false,
+        createdAt: new Date()
+    }
+}
+
+const findHighestID = () => {
+    let highestID = 0
+    todos.value.forEach((todo) => {
+        if (todo.id > highestID) {
+            highestID = todo.id
+        }
+    })
+    return highestID
+}
+
+watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+})
+
+watch(
+    todos,
+    (newVal) => {
+        localStorage.setItem('todos', JSON.stringify(newVal))
+    },
+    { deep: true }
+)
+
+onMounted(() => {
+    const nameFromStorage = localStorage.getItem('name')
+    if (nameFromStorage) {
+        name.value = nameFromStorage
+    }
+
+    const todosFromStorage = localStorage.getItem('todos')
+    if (todosFromStorage) {
+        todos.value = JSON.parse(todosFromStorage)
+    }
+})
+</script>
+
+<template>
+    <main class="app">
+        <section class="greeting">
+            <h2 class="title">
+                Hello, <input type="text" placeholder="Name here" v-model="name" />
+            </h2>
+        </section>
+        <section class="create-todo">
+            <h3>Create a todo</h3>
+            <form @submit.prevent="addToDo">
+                <h4>Whats on your todo list?</h4>
+                <input type="text" placeholder="Item 1" v-model="input_content" />
+                <input type="submit" value="Add todo" />
+            </form>
+        </section>
+    </main>
+</template>
